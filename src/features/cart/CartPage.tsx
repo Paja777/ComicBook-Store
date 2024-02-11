9e99eafa9e27ec9b8e5c6dbc4b932e34071e383d
Paja777 <pavle.rvovic95@gmail.com>
2024-02-11 20:44:33 +0100

    Finishing cart page

--- src/features/cart/CartPage.tsx
+++ src/features/cart/CartPage.tsx
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import Button from "../../app/components/Button";
 import { useAppSelector } from "../../app/store/ConfigureStore";
 import cartCover from "../../assets/cart cover.jpg";
@@ -7,24 +8,43 @@ import { useNavigate } from "react-router-dom";
 const CartPage = () => {
   const { cartItems } = useAppSelector((state) => state.account);
   const navigate = useNavigate();
+  const { user } = useAuth0();
+  const totalCost = cartItems.reduce((sum, item) => {
+    const price = parseInt(item.price.replace('$', ''), 10);
+    return sum + price;
+  },0)
+  console.log(user)
   const handleButtonClick = () => {
     navigate("/");
   };
   return (
-    <div className="relative h-[700px] text-red-600 overflow-hidden">
-      <div className="absolute top-10 left-[39%] font-poppins text-[50px]  opacity-45 text-gradient">
+    <div className="flex flex-col relative h-screen text-red-600 overflow-hidden">
+      <div className="absolute top-10 left-[42%] font-poppins text-[50px]  opacity-45 text-gradient">
         Your Cart
       </div>
-      
+      <div className="flex">
         <Button
           size={{ w: "75px", h: "100px" }}
           name="<- back"
           onHandleButtonClick={handleButtonClick}
         />
-      
+      </div>
       <img src={cartCover} alt="cover_image" className="opacity-20 absolute" />
-      <div className="mt-36 ">
-        <ProductList products={cartItems} />
+      <div className="flex flex-row mt-16 ">
+        <div className="w-[50%]">
+          <ProductList products={cartItems} />
+        </div>
+        <section className="flex flex-col font-poppins text-white text-[36px] mt-16 ml-10 leading-15">
+          <h2 className="text-gradient">Welcome To Your Cart</h2>
+          <h2>Total: ${totalCost}</h2>
+          <div className="flex mt-10">
+          <Button
+          size={{ w: "75px", h: "100px" }}
+          name="Finsih ->"
+          onHandleButtonClick={handleButtonClick}
+        />
+          </div>
+        </section>
       </div>
     </div>
   );
--- src/features/favorite/FavoritesPage.tsx
+++ src/features/favorite/FavoritesPage.tsx
@@ -1,17 +1,30 @@
+import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../app/store/ConfigureStore";
 import fav2 from "../../assets/fav2.webp";
 import ProductList from "../shop/ProductList";
+import Button from "../../app/components/Button";
 
 const FavoritesPage = () => {
   const { favorites } = useAppSelector((state) => state.account);
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate("/");
+  };
   return (
     <div className="relative h-[700px] text-red-600 overflow-hidden">
-      <div className="absolute top-10 left-[39%] font-poppins text-[50px] opacity-45 text-gradient">
+      <div className="absolute top-10 left-[40%] font-poppins text-[50px] opacity-45 text-gradient">
         Your Favorite
       </div>
+      <div className="">
+        <Button
+          size={{ w: "75px", h: "100px" }}
+          name="<- back"
+          onHandleButtonClick={handleButtonClick}
+        />
+      </div>
       <img src={fav2} alt="cover_image" className="opacity-20 absolute" />
-      <div>
-        <ProductList products={favorites}/>
+      <div className="mt-36">
+        <ProductList products={favorites} />
       </div>
     </div>
   );
